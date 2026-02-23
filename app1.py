8973a61ef4f23c85ebc171793386b984cd720137
52862248 <geaf.manuel@gmail.com>
2026-02-23 15:45:49 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -241,7 +241,7 @@ else:
 
         # Pulsante per scaricare il database
         if role == "admin":
-            st.subheader("Scarica database aggiornato")
+            st.subheader("Gestione del database")
 
             # apriamo il file in modalità binaria
             with open("courses.db", "rb") as f:
@@ -269,4 +269,5 @@ if st.session_state["db_changed"]:
 
         st.session_state["last_backup"] = now
         st.session_state["db_changed"] = False
+
 
