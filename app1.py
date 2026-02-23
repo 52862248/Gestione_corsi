98a612a2b8ad2fad9a05812e0e05fea7511fa42d
52862248 <geaf.manuel@gmail.com>
2026-02-23 09:28:35 +0100

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,199 @@
+import streamlit as st
+import sqlite3
+
+conn = sqlite3.connect("courses.db", check_same_thread=False)
+c = conn.cursor()
+
+# Tabelle
+c.execute("""CREATE TABLE IF NOT EXISTS users(
+id INTEGER PRIMARY KEY,
+username TEXT,
+password TEXT,
+role TEXT
+)""")
+
+c.execute("""CREATE TABLE IF NOT EXISTS courses(
+id INTEGER PRIMARY KEY,
+title TEXT
+)""")
+
+c.execute("""CREATE TABLE IF NOT EXISTS enrollments(
+id INTEGER PRIMARY KEY,
+user_id INTEGER,
+course_id INTEGER
+)""")
+
+conn.commit()
+
+st.title("Gestione corsi")
+
+# LOGIN
+if "user" not in st.session_state:
+
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+
+    if st.button("Login"):
+
+        user = c.execute(
+        "SELECT * FROM users WHERE username=? AND password=?",
+        (username,password)).fetchone()
+
+        if user:
+            st.session_state.user = user
+            st.rerun()
+        else:
+            st.error("Credenziali errate")
+
+else:
+
+    user = st.session_state.user
+    role = user[3]
+
+    st.sidebar.write("Utente:", user[1])
+
+    if st.sidebar.button("Logout"):
+        del st.session_state.user
+        st.rerun()
+
+    menu = ["Corsi disponibili","I miei corsi"]
+
+    if role == "admin":
+        menu += ["Crea corso", "Lista iscritti", "Gestione utenti"]
+
+    choice = st.sidebar.selectbox("Menu", menu)
+
+    # ----------------------
+
+    if choice == "Corsi disponibili":
+
+        courses = c.execute("SELECT * FROM courses").fetchall()
+
+        for course in courses:
+
+            st.subheader(course[1])
+
+            already = c.execute("""
+            SELECT * FROM enrollments
+            WHERE user_id=? AND course_id=?
+            """, (user[0], course[0])).fetchone()
+
+            if already:
+                st.info("Sei già iscritto")
+            else:
+                if st.button("Iscriviti", key=course[0]):
+                    c.execute(
+                        "INSERT INTO enrollments(user_id,course_id) VALUES (?,?)",
+                        (user[0], course[0])
+                    )
+
+                    conn.commit()
+                    st.success("Iscritto")
+
+    # ----------------------
+
+    if choice == "I miei corsi":
+
+        courses = c.execute("""
+        SELECT courses.id, courses.title
+        FROM courses
+        JOIN enrollments
+        ON courses.id=enrollments.course_id
+        WHERE enrollments.user_id=?
+        """,(user[0],)).fetchall()
+
+        for course in courses:
+
+            st.write(course[1])
+
+            if st.button("Cancella iscrizione", key="del"+str(course[0])):
+
+                c.execute("""
+                DELETE FROM enrollments
+                WHERE user_id=? AND course_id=?
+                """,(user[0],course[0]))
+
+                conn.commit()
+
+                st.success("Iscrizione cancellata")
+
+    # ----------------------
+
+    if choice == "Crea corso":
+
+        title = st.text_input("Titolo corso")
+
+        if st.button("Salva"):
+            c.execute("INSERT INTO courses(title) VALUES (?)",(title,))
+            conn.commit()
+
+            st.success("Corso creato")
+
+    # ----------------------
+
+    if choice == "Lista iscritti":
+
+        courses = c.execute("SELECT * FROM courses").fetchall()
+
+        for course in courses:
+
+            st.subheader(course[1])
+
+            users = c.execute("""
+            SELECT users.username
+            FROM enrollments
+            JOIN users ON users.id=enrollments.user_id
+            WHERE enrollments.course_id=?
+            """, (course[0],)).fetchall()
+
+            if users:
+                for u in users:
+                    st.write("-", u[0])
+            else:
+                st.write("--- Nessun iscritto ---")
+
+####### disabilitato
+#      data = c.execute("""
+#        SELECT users.username, courses.title
+#        FROM enrollments
+#        JOIN users ON users.id=enrollments.user_id
+#        JOIN courses ON courses.id=enrollments.course_id
+#        """).fetchall()
+
+#        for row in data:
+#            st.write(row[0], "→", row[1])
+#            ############
+
+
+    if choice == "Gestione utenti":
+
+        st.subheader("Crea nuovo utente")
+
+        new_user = st.text_input("Username")
+        new_pass = st.text_input("Password", type="password")
+
+        role_user = st.selectbox("Ruolo", ["user", "admin"])
+
+        if st.button("Crea utente"):
+            c.execute(
+                "INSERT INTO users(username,password,role) VALUES (?,?,?)",
+                (new_user, new_pass, role_user)
+            )
+
+            conn.commit()
+            st.success("Utente creato")
+
+        st.subheader("Elimina utente")
+
+        users = c.execute("SELECT id,username FROM users").fetchall()
+
+        user_dict = {u[1]:u[0] for u in users}
+
+        selected = st.selectbox("Seleziona utente",user_dict.keys())
+
+        if st.button("Elimina utente"):
+
+            c.execute("DELETE FROM users WHERE id=?",(user_dict[selected],))
+            conn.commit()
+
+            st.warning("Utente eliminato")
