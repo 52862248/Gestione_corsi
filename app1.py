b11863a7a9c9e3c178c49eaa3d148f8c6875f595
52862248 <geaf.manuel@gmail.com>
2026-02-24 09:08:26 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -106,7 +106,7 @@ else:
     # ----------------------
 
     if choice == "Corsi disponibili":
-
+        title = st.text_input("Corsi disponibili")
         courses = c.execute("SELECT * FROM courses").fetchall()
 
         for course in courses:
@@ -134,7 +134,7 @@ else:
     # ----------------------
 
     if choice == "Iscrizioni attivate":
-
+        title = st.text_input("Iscrizioni attivate")
         courses = c.execute("""
         SELECT courses.id, courses.title
         FROM courses
@@ -268,6 +268,7 @@ else:
 
         
        
+
 
 
 
